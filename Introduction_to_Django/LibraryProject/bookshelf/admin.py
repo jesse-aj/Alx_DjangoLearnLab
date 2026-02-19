ab444f85974820045c917a5d47ab234963ac1634
jesse-aj <appiahjesse123@gmail.com>
2026-02-19 00:35:28 +0000

    Registered book Model with the Django Admin page

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_view = ("title", "author", "publication_year")
+    search_fields = ("title", "author", "publication_year")
+    list_filter = ("publication_year", )
+
+
+admin.site.register(Book, BookAdmin)
