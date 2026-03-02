664883d5dbe964ac68dc7ed6c787a43172ee7ab3
jesse-aj <appiahjesse123@gmail.com>
2026-03-02 04:19:44 +0000

    fixed errors in urls_relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import list_books, LibraryDetailView, register, admin_view, librarian_view, member_view
 
 urlpatterns = [
     # Home -> books list
