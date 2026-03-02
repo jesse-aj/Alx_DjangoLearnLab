2cee60b8befb72fcad90ca897eb13b02c2cccd90
jesse-aj <appiahjesse123@gmail.com>
2026-03-02 04:15:46 +0000

    fixed errors in views_relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library
+from .models import Book
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
