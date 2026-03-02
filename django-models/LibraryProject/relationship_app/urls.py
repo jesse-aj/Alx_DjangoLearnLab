d6cf8ceefbc24d94377f39bc43d9748d345d9923
jesse-aj <appiahjesse123@gmail.com>
2026-03-02 04:29:48 +0000

    fixed errors in views_relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
 
     # Authentication
     path('login/', auth_views.LoginView.as_view(
-        template_name='relationship_app/login.html'), name='login'),
+        template_name="relationship_app/login.html"), name='login'),
 
     path('logout/', auth_views.LogoutView.as_view(
-        template_name='relationship_app/logout.html'), name='logout'),
+        template_name="relationship_app/logout.html"), name='logout'),
 
     path('register/', views.register, name='register'),
 
